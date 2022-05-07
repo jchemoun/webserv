205d71d1edf613fbaa9d8b9e6829a431c2d03c0e
mjacq <mjacq@redoute.fr>
2022-05-07 17:36:07 +0200

    request parse header (first draft)

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -6,39 +6,74 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 13:17:12 by jchemoun          #+#    #+#             */
-/*   Updated: 2022/05/03 08:11:46 by mjacq            ###   ########.fr       */
+/*   Updated: 2022/05/07 17:31:15 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
-Request::Request(/* args */): unparsed_request()
+// Request::Request(std::stringstream &stream): stream(stream), is_complete(false)
+Request::Request(): is_complete(false)
 {
+	(void)is_complete;
 }
 
 bool	Request::parse_request()
 {
-	size_t	start;
-	size_t	end;
+	std::stringstream	stream;
+	std::stringstream	header_stream;
+	std::string			line;
+	std::string			key;
+	std::string			value;
 
-	start = 0;
-	end = unparsed_request.find_first_of(' ', 0);
-	method = unparsed_request.substr(0, end);
-	std::cout << unparsed_request << '\n';
-	//if (method != "GET" || method != "POST" || method != "DELETE")
-	//	return (false);
-	std::cout << start << ':' << end << " size: " << method.size() << " method: " << method << '\n';
-	start = ++end;
-	end = unparsed_request.find_first_of(' ', start);
-	location = unparsed_request.substr(start, end - start);
-	// std::cout << location.size() << location << '\n';
-	std::cout << start << ':' << end << " size: " << location.size() << " location: " << location << '\n';
-	unparsed_request.clear();
+	if (unparsed_request.find("\r\n\r\n") != std::string::npos || unparsed_request.find("\n\n") != std::string::npos)
+		std::cout << "\e[32mComplete request\e[0m\n";
+	else
+		std::cout << "\e[31mIncomplete request\e[0m\n";
+
+	stream << unparsed_request;				std::cout << "\e[32m" << unparsed_request << "\e[0m✋\n";
+	stream.seekg(0);						//std::cout << "\e[32m" << stream.str() << "\e[0m✋" << std::endl;
+	std::getline(stream, method, ' ');		std::cout << "method: " << method << "\n";
+	std::getline(stream, location, ' ');	std::cout << "location: " << location << "\n";
+	std::getline(stream, protocol);			std::cout << "protocol: " << protocol << "\n";
+	while (stream.good()) {
+		header_stream.clear();
+		std::getline(stream, line);
+		if (header_stream.good() && !line.empty() && line != "\r") {
+			header_stream << line;
+			header_stream.seekg(0);
+			if (header_stream.good()) {
+				std::getline(header_stream, key, ':');
+				std::getline(header_stream, value);
+				header[key] = value;
+				std::cout << "\e[36m" << key << ":" << header[key] << "\e[0m\n";
+			}
+		}
+	}
+	// if (false) {
+	// 	size_t	start;
+	// 	size_t	end;
+	//
+	// 	start = 0;
+	// 	end = unparsed_request.find_first_of(' ', 0);
+	// 	method = unparsed_request.substr(0, end);
+	// 	std::cout << unparsed_request << '\n';
+	// 	//if (method != "GET" || method != "POST" || method != "DELETE")
+	// 	//	return (false);
+	// 	std::cout << start << ':' << end << " size: " << method.size() << " method: " << method << '\n';
+	// 	start = ++end;
+	// 	end = unparsed_request.find_first_of(' ', start);
+	// 	location = unparsed_request.substr(start, end - start);
+	// 	// std::cout << location.size() << location << '\n';
+	// 	std::cout << start << ':' << end << " size: " << location.size() << " location: " << location << '\n';
+	// 	unparsed_request.clear();
+	// }
 	return (true);
 }
 
 void	Request::append_unparsed_request(char *buffer, ssize_t len)
 {
+	// stream.write(buffer, len);
 	unparsed_request.append(buffer, len);
 }
 
@@ -46,6 +81,4 @@ std::string const	&Request::get_location() const {
 	return (location);
 }
 
-Request::~Request()
-{
-}
+Request::~Request() { }
--- src/server/Request.hpp
+++ src/server/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: mjacq <mjacq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 13:16:09 by jchemoun          #+#    #+#             */
-/*   Updated: 2022/05/03 08:11:27 by mjacq            ###   ########.fr       */
+/*   Updated: 2022/05/07 17:27:30 by mjacq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@
 # include <string>
 # include <sys/types.h>
 # include <iostream>
+# include <map>
+# include <sstream>
 
 class Request
 {
 private:
-	std::string	unparsed_request;
-	std::string	method;
-	std::string	location;
+	// std::stringstream					&stream;
+	std::string							unparsed_request;
+	std::string							method;
+	std::string							location;
+	std::string							protocol;
+	std::map<std::string, std::string>	header;
+	bool								is_complete; // when \n\n
+	// std::string							error;
 public:
-	Request(/* args */);
+	Request();
 
 	bool	parse_request();
 	void	append_unparsed_request(char *buffer, ssize_t len);
