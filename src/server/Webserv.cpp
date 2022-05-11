741448d6a690aacde5843870eeba12024ae74f24
jchemoun <jchemoun@student.42.fr>
2022-05-11 14:15:11 +0200

    fix weird master

--- src/server/Webserv.cpp
+++ src/server/Webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 13:17:02 by jchemoun          #+#    #+#             */
-/*   Updated: 2022/05/11 14:25:22 by user42           ###   ########.fr       */
+/*   Updated: 2022/05/11 14:27:58 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,8 +162,12 @@ bool	Webserv::handle_send(int client_fd)
 	// need to get right server to response, todo after merge of 2 class config
 	// need to create header, todo after looking at nginx response header && merge of class config
 
-	send(client_fd, response.c_str(), response.size(), 0); // TODO: what to do in case of send error
-	epoll_event	event = { };
+	if (send(client_fd, response.c_str(), response.size(), 0) < 0) // TODO: what to do in case of send error
+	{
+		std::cerr << "error send\n";
+		return (false);
+	}
+	epoll_event event = { };
 	event.events = EPOLLIN;
 	event.data.fd = client_fd;
 	if (epoll_ctl(epfd, EPOLL_CTL_MOD, client_fd, &event) < 0)
