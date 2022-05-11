87dc7ca0f59ba365f2124efa7cd96380b7b57e61
mjacq <mjacq@redoute.fr>
2022-05-11 18:53:47 +0200

    test multiple connections by multiple clients simultaneously

--- conf/multiple_connections.sh
+++ conf/multiple_connections.sh
@@ -1,4 +1,7 @@
-# Five client simultaneously
+#! /usr/bin/env zsh
+
+# ============================= Multiple clients ============================= #
+
 cat \
   <(curl localhost:8080) \
   <(curl localhost:8080/folder/) \
@@ -7,20 +10,58 @@ cat \
   <(curl localhost:8080/not_found) \
   | sed 's/nginx[^<]*/webserv\/0.1/g' | sed 's/\s\+/ /g' | sed 's/[0-9]\+:[0-9]\+/hour:min/' | sed 's/ $//'
 
-# Same client but multiple packets in a raw
-{ sleep 0.5;
-  echo GET / HTTP/1.1          ; sleep 0.1 ; echo Host: localhost:8080 ; sleep 0.1 ; echo ; sleep 0.1 ;
-  echo GET /folder/ HTTP/1.1   ; sleep 0.1 ; echo Host: localhost:8080 ; sleep 0.1 ; echo ; sleep 0.1 ;
-  echo GET /not_found HTTP/1.1 ; sleep 0.1 ; echo Host: localhost:8080 ; sleep 0.1 ; echo ; sleep 0.1 ;
-  echo GET / HTTP/1.1          ; sleep 0.1 ; echo Host: localhost:8080 ; sleep 0.1 ; echo ; sleep 0.1 ;
-} | telnet localhost 8080 |
-  sed 's/nginx[^<]*/webserv\/0.1/g' |
-  sed 's/\s\+/ /g' |
-  sed 's/[0-9]\+:[0-9]\+/hour:min/' |
-  sed 's/ $//' |
-  sed '/^\S\+:.*$/d' |            # remove headers
-  sed '/^[0-9]*$/d' | sed '/^$/d' # remove chuck encoding lines
+# =================== Single client with multiple requests =================== #
 
+multi_get() {
+  delay="$1"
+  shift
+  { sleep 0.5;
+    for uri in $@; do
+      echo GET $uri HTTP/1.1 ; sleep $delay ; echo Host: localhost:8080 ; sleep $delay ; echo ; sleep 0.1 ;
+    done;
+  } | telnet localhost 8080 |
+    sed 's/nginx[^<]*/webserv\/0.1/g' |
+    sed 's/\s\+/ /g' |
+    sed 's/[0-9]\+:[0-9]\+/hour:min/' |
+    sed 's/ $//' |
+    sed '/^\S\+:.*$/d' |            # remove headers
+    sed '/^[0-9]*$/d' | sed '/^$/d' # remove chuck encoding lines
+}
+
+multi_get 0.02 / /foler/ /not_found /
+
+# ================= Multiple clients with multiple requests ================== #
+
+cat \
+  <(multi_get 0.02 / /folder/ / /folder/ / /folder /not_found / /not_found /not_found /folder/) \
+  <(multi_get 0.02 /not_found / /folder/ / /folder/ / /folder /not_found / /not_found /not_found /folder/) \
+  <(multi_get 0.05 / /folder/ /subfolder/ /not_found / / /folder/) \
+  <(multi_get 0.02 / /not_found / /folder/ / /folder/ / /folder /not_found / /not_found /not_found /folder/) \
+  <(multi_get 0.07 /hello_world.html /folder / /) \
+  <(multi_get 0.1 /fds / /folder/)
+
+cat \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/) \
+  <(multi_get 0.01 /fds / /folder/)
 
 # create unknown epoll event (25) loop
 # expect <<- EOF
